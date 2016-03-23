6035fb2608a87e4b0bd50a7ca0e18b1c304f5614
Dima Kudosh <LagMegantic@yandex.ru>
2016-03-23 18:03:00 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name = 'pydfs-lineup-optimizer',
-    version = '0.3',
+    version = '0.4',
     packages = ['tests', 'pydfs_lineup_optimizer', 'pydfs_lineup_optimizer.app'],
     url = 'https://github.com/DimaKudosh/pydfs-lineup-optimizer',
     license = '',
@@ -18,6 +18,5 @@ setup(
           'Intended Audience :: Developers',
           'Operating System :: POSIX',
           'Programming Language :: Python',
-
           ],
 )
