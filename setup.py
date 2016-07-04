d2347ac81dd9ad1775681819a42f5c2a2b390697
Dima Kudosh <LagMegantic@yandex.ru>
2016-07-04 21:36:34 +0300

    Add test command to setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,18 @@
+import sys
 from distutils.core import setup
+from setuptools.command.test import test as TestCommand
+
+
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+        errno = pytest.main(self.test_args)
+        sys.exit(errno)
 
 setup(
     name = 'pydfs-lineup-optimizer',
@@ -11,6 +25,8 @@ setup(
     description = 'Tool for creating optimal lineups for daily fantasy sports',
     keywords = ['dfs', 'fantasy', 'sport', 'lineup', 'optimize', 'optimizer'],
     install_requires = ['PuLP'],
+    tests_require=['pytest'],
+    cmdclass={'test': PyTest},
     classifiers=[
           'Development Status :: 4 - Beta',
           'Environment :: Console',
