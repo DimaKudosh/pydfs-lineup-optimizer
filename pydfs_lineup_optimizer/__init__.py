ff1fcaf289bdf4933007692ee16485a0d0714d1d
Dima Kudosh <LagMegantic@yandex.ru>
2017-03-19 18:37:38 +0300

    Update version

--- pydfs_lineup_optimizer/__init__.py
+++ pydfs_lineup_optimizer/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.2"
+__version__ = "1.0"
 
 from .player import Player
 from .exceptions import LineupOptimizerException
--- setup.py
+++ setup.py
@@ -1,38 +1,22 @@
-import sys
 from distutils.core import setup
-from setuptools.command.test import test as TestCommand
-
-
-class PyTest(TestCommand):
-    def finalize_options(self):
-        TestCommand.finalize_options(self)
-        self.test_args = []
-        self.test_suite = True
-
-    def run_tests(self):
-        import pytest
-        errno = pytest.main(self.test_args)
-        sys.exit(errno)
 
 setup(
-    name = 'pydfs-lineup-optimizer',
-    version = '0.4.4',
-    packages = ['tests', 'pydfs_lineup_optimizer', 'pydfs_lineup_optimizer.app'],
-    url = 'https://github.com/DimaKudosh/pydfs-lineup-optimizer',
-    license = '',
-    author = 'Dima Kudosh',
-    author_email = 'dimakudosh@gmail.com',
-    description = 'Tool for creating optimal lineups for daily fantasy sports',
-    keywords = ['dfs', 'fantasy', 'sport', 'lineup', 'optimize', 'optimizer'],
-    install_requires = ['PuLP'],
-    tests_require=['pytest'],
-    cmdclass={'test': PyTest},
+    name='pydfs-lineup-optimizer',
+    version='1.0',
+    packages=['tests', 'pydfs_lineup_optimizer'],
+    url='https://github.com/DimaKudosh/pydfs-lineup-optimizer',
+    license='MIT',
+    author='Dima Kudosh',
+    author_email='dimakudosh@gmail.com',
+    description='Tool for creating optimal lineups for daily fantasy sports',
+    keywords=['dfs', 'fantasy', 'sport', 'lineup', 'optimize', 'optimizer'],
+    install_requires=['PuLP>=1.6.5'],
     classifiers=[
-          'Development Status :: 4 - Beta',
-          'Environment :: Console',
-          'Intended Audience :: End Users/Desktop',
-          'Intended Audience :: Developers',
-          'Operating System :: POSIX',
-          'Programming Language :: Python',
-          ],
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Developers',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+    ],
 )
