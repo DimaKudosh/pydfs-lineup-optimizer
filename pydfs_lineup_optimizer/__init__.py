d2ac7a45645fb614b603d9ba12ef300fec161a5e
Dima Kudosh <LagMegantic@yandex.ru>
2017-03-19 15:46:41 +0300

    Removed app

--- pydfs_lineup_optimizer/__init__.py
+++ pydfs_lineup_optimizer/__init__.py
@@ -4,8 +4,3 @@ from .player import Player
 from .exceptions import LineupOptimizerException
 from .settings import *
 from .lineup_optimizer import LineupOptimizer
-from .app.app import run
-
-
-def run_app():
-    run()
