09b6a184ab5ac815dc7152b86f3377e32bc4f0c0
Dima Kudosh <LagMegantic@yandex.ru>
2017-03-26 19:33:18 +0300

    Update version

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from distutils.core import setup
 
 setup(
     name='pydfs-lineup-optimizer',
-    version='1.0',
+    version='1.1',
     packages=['tests', 'pydfs_lineup_optimizer'],
     url='https://github.com/DimaKudosh/pydfs-lineup-optimizer',
     license='MIT',
     author='Dima Kudosh',
     author_email='dimakudosh@gmail.com',
     description='Tool for creating optimal lineups for daily fantasy sports',
-    keywords=['dfs', 'fantasy', 'sport', 'lineup', 'optimize', 'optimizer'],
+    keywords=['dfs', 'fantasy', 'sport', 'lineup', 'optimize', 'optimizer', 'nba', 'nfl', 'nhl', 'mlb'],
     install_requires=['PuLP>=1.6.5'],
     classifiers=[
         'Development Status :: 4 - Beta',
