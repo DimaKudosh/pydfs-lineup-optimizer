214da416b51e923267c07d8bdf0d2ea009d6df59
Eric Truett <sansbacon@gmail.com>
2018-11-29 09:10:33 -0600

    added importer for DK template file

--- pydfs_lineup_optimizer/sites/draftkings/importer.py
+++ pydfs_lineup_optimizer/sites/draftkings/importer.py
@@ -1,4 +1,5 @@
 import csv
+from itertools import islice
 from pydfs_lineup_optimizer.exceptions import LineupOptimizerIncorrectCSV
 from pydfs_lineup_optimizer.lineup_importer import CSVImporter
 from pydfs_lineup_optimizer.player import Player
@@ -10,25 +11,30 @@ from pydfs_lineup_optimizer.constants import Site
 class DraftKingsCSVImporter(CSVImporter):
     site = Site.DRAFTKINGS
 
-    def import_players(self):  # pragma: no cover
-        players = []
+    def _row_to_player(self, row):
+        try:
+            max_exposure = row.get('Max Exposure', '').replace('%', '')
+            name = row['Name'].split()
+            player = Player(
+                row['ID'],
+                name[0],
+                name[1] if len(name) > 1 else '',
+                row['Position'].split('/'),
+                row['TeamAbbrev'],
+                float(row['Salary']),
+                float(row['AvgPointsPerGame']),
+                max_exposure=float(max_exposure) if max_exposure else None
+            )
+        except KeyError:
+            raise LineupOptimizerIncorrectCSV
+        return player
+
+    def import_players(self, from_template=False):  # pragma: no cover
         with open(self.filename, 'r') as csvfile:
-            csv_data = csv.DictReader(csvfile, skipinitialspace=True)
-            for row in csv_data:
-                try:
-                    max_exposure = row.get('Max Exposure')
-                    name = row['Name'].split()
-                    player = Player(
-                        row['ID'],
-                        name[0],
-                        name[1] if len(name) > 1 else '',
-                        row['Position'].split('/'),
-                        row['TeamAbbrev'],
-                        float(row['Salary']),
-                        float(row['AvgPointsPerGame']),
-                        max_exposure=float(max_exposure.replace('%', '')) if max_exposure else None
-                    )
-                except KeyError:
-                    raise LineupOptimizerIncorrectCSV
-                players.append(player)
+            if from_template:
+                csv_data = csv.DictReader(islice(csvfile, 7, None),
+                                          skipinitialspace=True)
+            else:
+                csv_data = csv.DictReader(csvfile, skipinitialspace=True)
+            players = [self._row_to_player(row) for row in csv_data]
         return players
