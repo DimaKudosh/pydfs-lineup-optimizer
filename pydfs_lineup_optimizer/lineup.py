abf8ef9dc20f2570327ea39bdd04776fdd0f4983
Dima Kudosh <LagMegantic@yandex.ru>
2018-12-23 15:49:28 +0300

    #34 Added lineup printer

--- pydfs_lineup_optimizer/lineup.py
+++ pydfs_lineup_optimizer/lineup.py
@@ -1,11 +1,13 @@
-from typing import List
+from typing import List, Type
 from pydfs_lineup_optimizer.player import LineupPlayer
+from pydfs_lineup_optimizer.lineup_printer import BaseLineupPrinter, LineupPrinter
 
 
 class Lineup(object):
-    def __init__(self, players):
-        # type: (List[LineupPlayer]) -> None
+    def __init__(self, players, printer=LineupPrinter):
+        # type: (List[LineupPlayer], Type[BaseLineupPrinter]) -> None
         self.players = players
+        self.printer = printer()
 
     def __iter__(self):
         return iter(self.players)
@@ -14,20 +16,7 @@ class Lineup(object):
         return item in self.players
 
     def __str__(self):
-        res = ''
-        for index, player in enumerate(self.players, start=1):
-            res += '{0:>2}. {1:<5} {2:<30}{3:<6}{4:<15}{5:<8}{6:<10}\n'.format(
-                index,
-                player.lineup_position,
-                player.full_name,
-                '/'.join(player.positions),
-                player.team,
-                round(player.fppg, 3),
-                str(player.salary) + '$',
-            )
-        res += '\nFantasy Points ' + str(self.fantasy_points_projection)
-        res += '\nSalary ' + str(self.salary_costs)
-        return res
+        return self.printer.print_lineup(self)
 
     def __repr__(self):
         return 'Lineup: projection %s, budget %s' % (self.fantasy_points_projection, self.salary_costs)
