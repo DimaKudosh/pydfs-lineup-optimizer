d12fce761ac212c3bba87f830f43cc2bc35b4891
Dima Kudosh <LagMegantic@yandex.ru>
2019-03-09 18:50:53 +0300

    #42 Fixed settings for FantasyDraft Golf

--- pydfs_lineup_optimizer/sites/fantasy_draft/settings.py
+++ pydfs_lineup_optimizer/sites/fantasy_draft/settings.py
@@ -1,6 +1,7 @@
 from pydfs_lineup_optimizer.settings import BaseSettings, LineupPosition
 from pydfs_lineup_optimizer.constants import Sport, Site
 from pydfs_lineup_optimizer.sites.sites_registry import SitesRegistry
+from pydfs_lineup_optimizer.lineup_printer import IndividualSportLineupPrinter
 
 
 class FantasyDraftSettings(BaseSettings):
@@ -75,7 +76,10 @@ class FantasyDraftBaseballSettings(FantasyDraftSettings):
 @SitesRegistry.register_settings
 class FantasyDraftGolfSettings(FantasyDraftSettings):
     sport = Sport.GOLF
+    max_from_one_team = None
+    lineup_printer = IndividualSportLineupPrinter
     positions = [
+        LineupPosition('G', ('G', )),
         LineupPosition('G', ('G', )),
         LineupPosition('G', ('G', )),
         LineupPosition('G', ('G', )),
