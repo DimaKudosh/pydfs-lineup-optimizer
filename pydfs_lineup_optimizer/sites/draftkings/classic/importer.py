143c0429485e34663ac7eb15b1b3e8fc0cb5fde9
Dima Kudosh <LagMegantic@yandex.ru>
2019-06-05 21:13:58 +0300

    Fixed game info parsing for DK MMA

--- pydfs_lineup_optimizer/sites/draftkings/classic/importer.py
+++ pydfs_lineup_optimizer/sites/draftkings/classic/importer.py
@@ -26,9 +26,9 @@ class DraftKingsCSVImporter(CSVImporter):  # pragma: nocover
                 starts_at='',
                 game_started=True)
         try:
-            teams, date = game_info.split(' ', 1)
-            away_team, home_team = teams.split('@')
-            starts_at = datetime.strptime(date.replace(' ET', ''), '%m/%d/%Y %I:%M%p').\
+            teams, date, time, tz = game_info.rsplit(' ', 3)
+            away_team, home_team = teams.upper().split('@')
+            starts_at = datetime.strptime(date + time, '%m/%d/%Y%I:%M%p').\
                 replace(tzinfo=timezone(self.DEFAULT_TIMEZONE))
             return GameInfo(
                 home_team=home_team,
