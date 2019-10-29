7e21b3ad07e061360e731816bf7cc01aac5df6de
Dima Kudosh <LagMegantic@yandex.ru>
2019-10-29 15:23:40 +0300

    #92 Added FanDuel Golf

--- pydfs_lineup_optimizer/sites/fanduel/classic/settings.py
+++ pydfs_lineup_optimizer/sites/fanduel/classic/settings.py
@@ -2,7 +2,7 @@ from typing import List, Type
 from pydfs_lineup_optimizer.settings import BaseSettings, LineupPosition
 from pydfs_lineup_optimizer.constants import Sport, Site
 from pydfs_lineup_optimizer.sites.sites_registry import SitesRegistry
-from pydfs_lineup_optimizer.lineup_printer import DropLowestLineupPrinter
+from pydfs_lineup_optimizer.lineup_printer import DropLowestLineupPrinter, IndividualSportLineupPrinter
 from pydfs_lineup_optimizer.rules import OptimizerRule, FanduelBaseballRosterRule, FanduelMinimumTeamsRule
 
 
@@ -95,4 +95,20 @@ class FanDuelWnbaSettings(FanDuelSettings):
         LineupPosition('F', ('F', )),
         LineupPosition('F', ('F', )),
         LineupPosition('F', ('F', )),
+    ]
+
+
+@SitesRegistry.register_settings
+class FanDuelGolfSettings(FanDuelSettings):
+    sport = Sport.GOLF
+    max_from_one_team = None
+    extra_rules = []  # type: List[Type[OptimizerRule]]
+    lineup_printer = IndividualSportLineupPrinter
+    positions = [
+        LineupPosition('G', ('G', )),
+        LineupPosition('G', ('G', )),
+        LineupPosition('G', ('G', )),
+        LineupPosition('G', ('G', )),
+        LineupPosition('G', ('G', )),
+        LineupPosition('G', ('G', )),
     ]
