af6d0a4ebffc5c56f415585deaf9a61af6cf2876
Raymond Huynh <raymond.huynh@klarna.com>
2020-03-18 20:46:19 +0000

    commit

--- example.py
+++ example.py
@@ -3,10 +3,23 @@ from pydfs_lineup_optimizer import Site, Sport, get_optimizer
 import pandas as pd
 
 optimizer = get_optimizer(Site.FANDUEL, Sport.FOOTBALL)
+optimizer.load_players_from_csv("/Users/raymond.huynh/Desktop/python/Ray_Python/data/fantasy/nrl_data.csv")
+lineup_generator = optimizer.optimize(20)
+for lineup in lineup_generator:
+    print(lineup)
+
+
+
+# %%
+from pydfs_lineup_optimizer import Site, Sport, get_optimizer
+import pandas as pd
+
+optimizer = get_optimizer(Site.FANDUEL, Sport.FOOTBALL)
 optimizer.load_players_from_csv("/Users/raymond.huynh/Desktop/python/Ray_Python/data/fantasy/afl_data.csv")
 lineup_generator = optimizer.optimize(20)
 for lineup in lineup_generator:
     print(lineup)
+
 
 
 
