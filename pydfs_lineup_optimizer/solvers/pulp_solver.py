1b41499cd220282bd6a492be30c3cccfe4a1c4f1
Eric Truett <etruett@alas.com>
2020-10-01 16:20:31 -0500

    Added try/except block to pulp_solver PULP_CBC_CMD

--- pydfs_lineup_optimizer/solvers/pulp_solver.py
+++ pydfs_lineup_optimizer/solvers/pulp_solver.py
@@ -1,29 +1,52 @@
-from pulp import LpProblem, LpMaximize, LpVariable, lpSum, LpStatusOptimal, LpBinary, LpInteger, PULP_CBC_CMD
+from pulp import (
+    LpProblem,
+    LpMaximize,
+    LpVariable,
+    lpSum,
+    LpStatusOptimal,
+    LpBinary,
+    LpInteger,
+    PULP_CBC_CMD,
+)
 from .base import Solver
 from .constants import SolverSign
 from .exceptions import SolverException
 
 
 class PuLPSolver(Solver):
-    LP_SOLVER = PULP_CBC_CMD(verbose=False, msg=False)
+    # fixes bug due to PULP_CBC_CMB removing verbose argument
+    try:
+        LP_SOLVER = PULP_CBC_CMD(verbose=False, msg=False)
+    except TypeError:
+        LP_SOLVER = PULP_CBC_CMD(msg=False)
 
     def __init__(self):
-        self.prob = LpProblem('Daily Fantasy Sports', LpMaximize)
+        self.prob = LpProblem("Daily Fantasy Sports", LpMaximize)
 
     def setup_solver(self):
         pass
 
     def add_variable(self, name, min_value=None, max_value=None):
         if any([min_value, max_value]):
-            return LpVariable(name, lowBound=min_value, upBound=max_value, cat=LpInteger)
+            return LpVariable(
+                name, lowBound=min_value, upBound=max_value, cat=LpInteger
+            )
         return LpVariable(name, cat=LpBinary)
 
     def set_objective(self, variables, coefficients):
-        self.prob += lpSum([variable * coefficient for variable, coefficient in zip(variables, coefficients)])
+        self.prob += lpSum(
+            [
+                variable * coefficient
+                for variable, coefficient in zip(variables, coefficients)
+            ]
+        )
 
     def add_constraint(self, variables, coefficients, sign, rhs):
         if coefficients:
-            lhs = [variable * coefficient for variable, coefficient in zip(variables, coefficients)]
+            lhs = [
+                variable * coefficient
+                for variable, coefficient in zip(variables, coefficients)
+            ]
         else:
             lhs = variables
         if sign == SolverSign.EQ:
@@ -35,7 +58,7 @@ class PuLPSolver(Solver):
         elif sign == SolverSign.LTE:
             self.prob += lpSum(lhs) <= rhs
         else:
-            raise SolverException('Incorrect constraint sign')
+            raise SolverException("Incorrect constraint sign")
 
     def copy(self):
         new_solver = type(self)()
@@ -52,4 +75,4 @@ class PuLPSolver(Solver):
                     result.append(variable)
             return result
         else:
-            raise SolverException('Unable to solve')
+            raise SolverException("Unable to solve")
